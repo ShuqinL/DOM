9be4d3ac6d57a27eb5a504e143f382d0562fc141
CCC <chaocheungchoi@gmail.com>
2019-01-24 23:00:20 +0100

    Update script.js

--- Read-Fix-Write/02-FIX/oef05/js/script.js
+++ Read-Fix-Write/02-FIX/oef05/js/script.js
@@ -5,19 +5,17 @@
  */
 
 const episodes = document.querySelectorAll(`.episode-item`);
-var $i = 1;
-episodes.forEach(($episode,$number) => {
+
+episodes.forEach(($episode, $number) => {
   const viewers = $episode.querySelector(`.episode-viewers`).textContent;
 
   const $meta =  $episode.querySelector(`.episode-meta`);
-  console.log("$meta: " + $meta);
-  console.log("$i: " + $i);
-  
-  $meta.innerHTML = `<a href="episode$number.mp4">Watch now</a>`;
+    
+  $meta.innerHTML = "<a href=\"episode" + $number + ".mp4\">Watch now</a>";
 
   const $air =  $episode.querySelector(`.episode-air`);
   if (Date.parse($air)<=Date.now()){
     $meta.classList.add("active");
   } 
-  i++;
+  
 });
