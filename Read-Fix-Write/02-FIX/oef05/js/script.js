ebd0c7ba50733dc726e19078d699fd30c690c29b
CCC <chaocheungchoi@gmail.com>
2019-01-24 22:19:20 +0100

    where did you declare your $number  ?

--- Read-Fix-Write/02-FIX/oef05/js/script.js
+++ Read-Fix-Write/02-FIX/oef05/js/script.js
@@ -5,17 +5,17 @@
  */
 
 const episodes = document.querySelectorAll(`.episode-item`);
-
+var i = 1;
 episodes.forEach($episode => {
   const viewers = $episode.querySelector(`.episode-viewers`).textContent;
 
   const $meta =  $episode.querySelector(`.episode-meta`);
 
-  $meta.innerHTML = `<a href=\"episode$meta.mp4\">Watch now</a>`;
+  $meta.innerHTML = `<a href=\"episode$i.mp4\">Watch now</a>`;
 
   const $air =  $episode.querySelector(`.episode-air`);
   if (Date.parse($air)<=Date.now()){
     $meta.classList.add("active");
   } 
-  
+  i++;
 });
